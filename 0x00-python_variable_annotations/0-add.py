98a933821229ee9e2232756222421730ea2b9fdd
peter-sigma <petergichehacrypto@gmail.com>
2024-07-04 09:13:39 +0000

    1. Basic annotations - concat

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,7 +1,15 @@
-"""function add :
-    a : float
-    b : float
-return: sum of a and b
+#!/usr/bin/env python3
 """
+add a and b
+"""
+
+
 def add(a: float, b: float) -> float:
+    """
+    function add
+    parameters:
+    a (float)
+    b (float)
+    return (float): a + b
+    """
     return (a+b)
---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+concatenate string
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    function concat
+
+    Parameters:
+    str1 (str): The first string.
+    str2 (str): The second string.
+
+    Return:
+    str: str1 + str2.
+    """
+    return str1 + str2
