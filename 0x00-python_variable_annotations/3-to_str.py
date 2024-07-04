ce09eae27794b3ad20c1288fd6eae5ce346019b5
peter-sigma <petergichehacrypto@gmail.com>
2024-07-04 12:04:34 +0000

    3. Basic annotations - to string

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""Write a type-annotated function to_str"""
+
+
+def to_str(n: float) -> str:
+    """
+    String representation of float
+
+    Parameters:
+        n (float): float to convert to string
+
+    Return
+        (str): string representation of a float
+    """
+    return str(n)
