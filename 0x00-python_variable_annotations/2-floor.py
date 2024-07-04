1e45878a7c0fdeda2e34350cee8e0fc03005fa49
peter-sigma <petergichehacrypto@gmail.com>
2024-07-04 09:26:12 +0000

    2. Basic annotations - floor

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+floor to remove decimal places
+"""
+
+
+def floor(n: float) -> int:
+    """
+    funtion floor to floor the floating point
+    parameters:
+    n (float): the floating number to floor
+
+    Return (int): an integer value corresponding to the floating number
+    """
+    return (int(n))
