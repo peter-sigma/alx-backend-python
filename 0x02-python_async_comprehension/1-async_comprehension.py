52ec6735c6b454bd2b7412e9606d51ee7214c640
peter-sigma <petergichehacrypto@gmail.com>
2024-07-09 07:23:42 +0000

    1. Async Comprehensions

---
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+   Async Comprehensions
+"""
+
+
+from typing import List
+
+
+async_generator = __import__('0-async_generator').async_generator
+
+
+async def async_comprehension() -> List[float]:
+    """
+       collect 10 random numbers using an async comprehension
+        using async_generator
+    """
+    return [i async for i in async_generator()]
