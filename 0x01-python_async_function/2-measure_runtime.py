d66f963b863994cef733bc99abd2da11de445337
peter-sigma <petergichehacrypto@gmail.com>
2024-07-09 00:48:57 +0000

    2. Measure the runtime

---
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""
+   Measure runtime of the coroutines
+"""
+import asyncio
+import time
+
+wait_n = __import__('1-concurrent_coroutines').wait_n
+
+
+def measure_time(n: int, max_delay: int) -> float:
+    """
+       measures the total execution time for wait_n(n, max_delay)
+    """
+    start_time = time.perf_counter()
+    asyncio.run(wait_n(n, max_delay))
+    total_time = time.perf_counter() - start_time
+
+    return total_time / n
