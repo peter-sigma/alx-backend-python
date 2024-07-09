33f5c019af69998a1d7308fc10f4de1c0c59a460
peter-sigma <petergichehacrypto@gmail.com>
2024-07-09 00:42:22 +0000

    1. Let's execute multiple coroutines at the same time with async

---
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""
+Chaining multiple coroutines 
+"""
+import asyncio
+from typing import List
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+async def wait_n(n: int, max_delay: int) -> List[float]:
+    """ 
+        A funtion to chain multiple coroutines 
+    """
+
+    delay_list = []
+    aws = [wait_random(max_delay) for i in range(n)]
+    for t in asyncio.as_completed(aws):
+        delay_list.append(await t)
+    return delay_list
